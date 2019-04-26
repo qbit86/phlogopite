2a3fe032abca23b2ce00c77a0efc193d74eb618f
Viktor T <qbit86@gmail.com>
2019-04-27 01:24:15 +0300

    Add Phlogopite.Abstractions project

---
+++ src/Phlogopite.Abstractions/Level.cs
@@ -0,0 +1,13 @@
+namespace Phlogopite
+{
+    public enum Level
+    {
+        Verbose = 0,
+        Debug,
+        Info,
+        Warning,
+        Error,
+        Fatal,
+        Silent
+    }
+}
