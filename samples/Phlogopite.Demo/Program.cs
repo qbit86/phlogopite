2981202c363b28e570c0ffdb1945d67a48d354d9
Viktor T <qbit86@gmail.com>
2019-04-29 02:30:13 +0300

    Add more logging examples

--- samples/Phlogopite.Demo/Program.cs
+++ samples/Phlogopite.Demo/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Globalization;
+using System.Threading;
 
 namespace Phlogopite
 {
@@ -6,6 +8,9 @@ namespace Phlogopite
     {
         private static void Main()
         {
+            // Messing with culture.
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("ru-RU");
+
             var sink = new ConsoleSink();
             var mediator = new Mediator();
             mediator.Add(sink);
@@ -16,9 +21,12 @@ namespace Phlogopite
         {
             const string tag = nameof(Program) + "." + nameof(Foo);
             var log = new Writer(mediator, tag);
-            log.V("Hello verbose", ("dateTime", DateTime.Now));
-            log.D("Anonymous property", (null, Environment.CurrentDirectory));
+            log.V("DateTime formatting", ("dateTime", DateTime.Now));
+            log.D("Anonymous property", (null, Thread.CurrentThread.CurrentCulture.Name));
             log.I("Here is float!", ("float", 1.618f));
+            log.W("Empty property", (null, null));
+            log.E("Error!", (null, new InvalidOperationException("Just test")));
+            log.F("This is fine.", ("integer", 1729u));
         }
     }
 }
