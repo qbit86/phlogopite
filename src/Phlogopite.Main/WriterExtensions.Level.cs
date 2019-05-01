c99435e28f5e4d7fe8084e21934031911d823b47
Viktor T <qbit86@gmail.com>
2019-05-01 15:13:39 +0300

    Implement generating *() methods for logging by level

--- src/Phlogopite.Main/WriterExtensions.Level.cs
+++ src/Phlogopite.Main/WriterExtensions.Level.cs
@@ -2,222 +2,268 @@ namespace Phlogopite
 {
     public static partial class WriterExtensions
     {
-#if false
         public static void V<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Verbose))
                 return;
+
+            WriteUnchecked(writer, Level.Verbose, text, p0, p1, p2, p3, p4);
         }
 
         public static void V<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4, in TProperty p5)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4, in NamedProperty p5)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Verbose))
                 return;
+
+            WriteUnchecked(writer, Level.Verbose, text, p0, p1, p2, p3, p4, p5);
         }
 
         public static void V<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4, in TProperty p5, in TProperty p6)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4, in NamedProperty p5, in NamedProperty p6)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Verbose))
                 return;
+
+            WriteUnchecked(writer, Level.Verbose, text, p0, p1, p2, p3, p4, p5, p6);
         }
 
         public static void V<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4, in TProperty p5, in TProperty p6, in TProperty p7)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4, in NamedProperty p5, in NamedProperty p6, in NamedProperty p7)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Verbose))
                 return;
+
+            WriteUnchecked(writer, Level.Verbose, text, p0, p1, p2, p3, p4, p5, p6, p7);
         }
 
         public static void D<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Debug))
                 return;
+
+            WriteUnchecked(writer, Level.Debug, text, p0, p1, p2, p3, p4);
         }
 
         public static void D<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4, in TProperty p5)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4, in NamedProperty p5)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Debug))
                 return;
+
+            WriteUnchecked(writer, Level.Debug, text, p0, p1, p2, p3, p4, p5);
         }
 
         public static void D<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4, in TProperty p5, in TProperty p6)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4, in NamedProperty p5, in NamedProperty p6)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Debug))
                 return;
+
+            WriteUnchecked(writer, Level.Debug, text, p0, p1, p2, p3, p4, p5, p6);
         }
 
         public static void D<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4, in TProperty p5, in TProperty p6, in TProperty p7)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4, in NamedProperty p5, in NamedProperty p6, in NamedProperty p7)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Debug))
                 return;
+
+            WriteUnchecked(writer, Level.Debug, text, p0, p1, p2, p3, p4, p5, p6, p7);
         }
 
         public static void I<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Info))
                 return;
+
+            WriteUnchecked(writer, Level.Info, text, p0, p1, p2, p3, p4);
         }
 
         public static void I<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4, in TProperty p5)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4, in NamedProperty p5)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Info))
                 return;
+
+            WriteUnchecked(writer, Level.Info, text, p0, p1, p2, p3, p4, p5);
         }
 
         public static void I<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4, in TProperty p5, in TProperty p6)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4, in NamedProperty p5, in NamedProperty p6)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Info))
                 return;
+
+            WriteUnchecked(writer, Level.Info, text, p0, p1, p2, p3, p4, p5, p6);
         }
 
         public static void I<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4, in TProperty p5, in TProperty p6, in TProperty p7)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4, in NamedProperty p5, in NamedProperty p6, in NamedProperty p7)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Info))
                 return;
+
+            WriteUnchecked(writer, Level.Info, text, p0, p1, p2, p3, p4, p5, p6, p7);
         }
 
         public static void W<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Warning))
                 return;
+
+            WriteUnchecked(writer, Level.Warning, text, p0, p1, p2, p3, p4);
         }
 
         public static void W<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4, in TProperty p5)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4, in NamedProperty p5)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Warning))
                 return;
+
+            WriteUnchecked(writer, Level.Warning, text, p0, p1, p2, p3, p4, p5);
         }
 
         public static void W<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4, in TProperty p5, in TProperty p6)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4, in NamedProperty p5, in NamedProperty p6)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Warning))
                 return;
+
+            WriteUnchecked(writer, Level.Warning, text, p0, p1, p2, p3, p4, p5, p6);
         }
 
         public static void W<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4, in TProperty p5, in TProperty p6, in TProperty p7)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4, in NamedProperty p5, in NamedProperty p6, in NamedProperty p7)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Warning))
                 return;
+
+            WriteUnchecked(writer, Level.Warning, text, p0, p1, p2, p3, p4, p5, p6, p7);
         }
 
         public static void E<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Error))
                 return;
+
+            WriteUnchecked(writer, Level.Error, text, p0, p1, p2, p3, p4);
         }
 
         public static void E<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4, in TProperty p5)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4, in NamedProperty p5)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Error))
                 return;
+
+            WriteUnchecked(writer, Level.Error, text, p0, p1, p2, p3, p4, p5);
         }
 
         public static void E<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4, in TProperty p5, in TProperty p6)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4, in NamedProperty p5, in NamedProperty p6)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Error))
                 return;
+
+            WriteUnchecked(writer, Level.Error, text, p0, p1, p2, p3, p4, p5, p6);
         }
 
         public static void E<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4, in TProperty p5, in TProperty p6, in TProperty p7)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4, in NamedProperty p5, in NamedProperty p6, in NamedProperty p7)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Error))
                 return;
+
+            WriteUnchecked(writer, Level.Error, text, p0, p1, p2, p3, p4, p5, p6, p7);
         }
 
         public static void F<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Fatal))
                 return;
+
+            WriteUnchecked(writer, Level.Fatal, text, p0, p1, p2, p3, p4);
         }
 
         public static void F<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4, in TProperty p5)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4, in NamedProperty p5)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Fatal))
                 return;
+
+            WriteUnchecked(writer, Level.Fatal, text, p0, p1, p2, p3, p4, p5);
         }
 
         public static void F<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4, in TProperty p5, in TProperty p6)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4, in NamedProperty p5, in NamedProperty p6)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Fatal))
                 return;
+
+            WriteUnchecked(writer, Level.Fatal, text, p0, p1, p2, p3, p4, p5, p6);
         }
 
         public static void F<TWriter>(this TWriter writer, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
-            in TProperty p4, in TProperty p5, in TProperty p6, in TProperty p7)
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            in NamedProperty p4, in NamedProperty p5, in NamedProperty p6, in NamedProperty p7)
             where TWriter : IWriter<NamedProperty>
         {
             if (!writer.IsEnabled(Level.Fatal))
                 return;
+
+            WriteUnchecked(writer, Level.Fatal, text, p0, p1, p2, p3, p4, p5, p6, p7);
         }
-#endif
     }
 }
