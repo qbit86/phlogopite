c337b1131dbd10db8b1c5d61a11576bfa5a48f82
Viktor T <qbit86@gmail.com>
2019-05-01 10:39:41 +0300

    Enhance template stub for generating *() methods for logging by level

--- src/Phlogopite.Main/WriterExtensions.Level.cs
+++ src/Phlogopite.Main/WriterExtensions.Level.cs
@@ -2,5 +2,174 @@ namespace Phlogopite
 {
     public static partial class WriterExtensions
     {
+#if false
+        public static void V<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void V<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4, in TProperty p5)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void V<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4, in TProperty p5, in TProperty p6)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void V<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4, in TProperty p5, in TProperty p6, in TProperty p7)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void D<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void D<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4, in TProperty p5)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void D<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4, in TProperty p5, in TProperty p6)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void D<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4, in TProperty p5, in TProperty p6, in TProperty p7)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void I<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void I<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4, in TProperty p5)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void I<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4, in TProperty p5, in TProperty p6)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void I<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4, in TProperty p5, in TProperty p6, in TProperty p7)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void W<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void W<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4, in TProperty p5)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void W<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4, in TProperty p5, in TProperty p6)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void W<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4, in TProperty p5, in TProperty p6, in TProperty p7)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void E<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void E<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4, in TProperty p5)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void E<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4, in TProperty p5, in TProperty p6)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void E<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4, in TProperty p5, in TProperty p6, in TProperty p7)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void F<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void F<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4, in TProperty p5)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void F<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4, in TProperty p5, in TProperty p6)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+
+        public static void F<TWriter>(this TWriter writer, string text,
+            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3,
+            in TProperty p4, in TProperty p5, in TProperty p6, in TProperty p7)
+            where TWriter : IWriter<NamedProperty>
+        {
+        }
+#endif
     }
 }
