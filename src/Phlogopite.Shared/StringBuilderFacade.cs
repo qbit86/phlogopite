7864bc106bc9ff64d242198368cd263671dc588a
Viktor T <qbit86@gmail.com>
2019-05-09 13:04:19 +0300

    Add conditional compilation symbols

--- src/Phlogopite.Shared/StringBuilderFacade.cs
+++ src/Phlogopite.Shared/StringBuilderFacade.cs
@@ -1,3 +1,11 @@
+#if NET20 || NET35 || NET40 || NET45 || NET451 || NET452 || NET46 || NET461 || NET462 || NET47 || NET471 || NET472 || NET48
+#define PHLOGOPITE_TRY_FORMAT_NOT_SUPPORTED
+#elif NETSTANDARD1_0 || NETSTANDARD1_1 || NETSTANDARD1_2 || NETSTANDARD1_3 || NETSTANDARD1_4 || NETSTANDARD1_5 || NETSTANDARD1_6 || NETSTANDARD2_0
+#define PHLOGOPITE_TRY_FORMAT_NOT_SUPPORTED
+#elif NETCOREAPP1_0 || NETCOREAPP1_1 || NETCOREAPP2_0
+#define PHLOGOPITE_TRY_FORMAT_NOT_SUPPORTED
+#endif
+
 using System;
 using System.Text;
 using static System.Diagnostics.Debug;
@@ -9,7 +17,7 @@ namespace Phlogopite
         private readonly StringBuilder _sb;
         private readonly IFormatProvider _formatProvider;
 
-        public StringBuilderFacade(StringBuilder sb, IFormatProvider formatProvider)
+        internal StringBuilderFacade(StringBuilder sb, IFormatProvider formatProvider)
         {
             Assert(sb != null);
             _sb = sb;
