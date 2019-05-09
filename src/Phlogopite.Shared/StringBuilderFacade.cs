a50a4590f203e5ab47fbaf955fe5e85fc65bc79e
Viktor T <qbit86@gmail.com>
2019-05-09 21:28:30 +0300

    Add Append(float value, ...) and Append(double value, ...)

--- src/Phlogopite.Shared/StringBuilderFacade.cs
+++ src/Phlogopite.Shared/StringBuilderFacade.cs
@@ -267,5 +267,61 @@ namespace Phlogopite
 
             _sb.Append(value.ToString(format, _formatProvider));
         }
+
+        internal void Append(float value)
+        {
+#if !PHLOGOPITE_TRY_FORMAT_NOT_SUPPORTED
+            Span<char> buffer = stackalloc char[CharStackBufferSize];
+            if (value.TryFormat(buffer, out int charsWritten, default, _formatProvider))
+            {
+                _sb.Append(buffer.Slice(0, charsWritten));
+                return;
+            }
+#endif
+
+            _sb.Append(value.ToString(_formatProvider));
+        }
+
+        internal void Append(float value, string format)
+        {
+#if !PHLOGOPITE_TRY_FORMAT_NOT_SUPPORTED
+            Span<char> buffer = stackalloc char[CharStackBufferSize];
+            if (value.TryFormat(buffer, out int charsWritten, format, _formatProvider))
+            {
+                _sb.Append(buffer.Slice(0, charsWritten));
+                return;
+            }
+#endif
+
+            _sb.Append(value.ToString(format, _formatProvider));
+        }
+
+        internal void Append(double value)
+        {
+#if !PHLOGOPITE_TRY_FORMAT_NOT_SUPPORTED
+            Span<char> buffer = stackalloc char[CharStackBufferSize];
+            if (value.TryFormat(buffer, out int charsWritten, default, _formatProvider))
+            {
+                _sb.Append(buffer.Slice(0, charsWritten));
+                return;
+            }
+#endif
+
+            _sb.Append(value.ToString(_formatProvider));
+        }
+
+        internal void Append(double value, string format)
+        {
+#if !PHLOGOPITE_TRY_FORMAT_NOT_SUPPORTED
+            Span<char> buffer = stackalloc char[CharStackBufferSize];
+            if (value.TryFormat(buffer, out int charsWritten, format, _formatProvider))
+            {
+                _sb.Append(buffer.Slice(0, charsWritten));
+                return;
+            }
+#endif
+
+            _sb.Append(value.ToString(format, _formatProvider));
+        }
     }
 }
