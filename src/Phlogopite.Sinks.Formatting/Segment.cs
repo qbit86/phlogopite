05effe2ef5dd3ad558846c546fcf763b33bcd4bf
Viktor T <qbit86@gmail.com>
2019-05-10 20:58:24 +0300

    Add Segment struct

---
+++ src/Phlogopite.Sinks.Formatting/Segment.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace Phlogopite
+{
+    public readonly struct Segment : IEquatable<Segment>
+    {
+        public Segment(int offset, int count)
+        {
+            Offset = offset;
+            Count = count;
+        }
+
+        public int Offset { get; }
+
+        public int Count { get; }
+
+        public bool Equals(Segment other)
+        {
+            return Offset == other.Offset && Count == other.Count;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Segment other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return unchecked(Offset * 397) ^ Count;
+        }
+
+        public static bool operator ==(Segment left, Segment right) => left.Equals(right);
+
+        public static bool operator !=(Segment left, Segment right) => !left.Equals(right);
+    }
+}
