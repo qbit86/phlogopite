d226e0d73b8bbeaf9f2c0a6b238edbdfc401e0ed
Viktor T <qbit86@gmail.com>
2019-05-11 01:31:54 +0300

    Implement rest of Format() method

--- src/Phlogopite.Sinks.Formatting/Formatter.cs
+++ src/Phlogopite.Sinks.Formatting/Formatter.cs
@@ -22,6 +22,135 @@ namespace Phlogopite
 
             RenderLevel(level, output);
             output.Append(" ");
+
+            var sbf = new StringBuilderFacade(output, formatProvider);
+
+            int timeIndex = FindDateTime(mediatorProperties, "time", out DateTime time);
+            if (timeIndex >= 0)
+            {
+                int timeOffset = output.Length;
+                RenderTime(time, sbf);
+                mediatorSegments[timeIndex] = new Segment(timeOffset, output.Length - timeOffset);
+            }
+            else
+            {
+                output.Append("            ");
+            }
+
+            output.Append(" ");
+
+            int tagIndex = FindString(writerProperties, "tag", out string tag);
+            int sourceIndex = FindString(writerProperties, "source", out string source);
+            if (tag != null || source != null)
+            {
+                output.Append("[");
+                if (!string.IsNullOrEmpty(tag))
+                {
+                    int tagOffset = output.Length;
+                    output.Append(tag);
+                    writerSegments[tagIndex] = new Segment(tagOffset, output.Length - tagOffset);
+                }
+
+                if (!string.IsNullOrEmpty(tag) && !string.IsNullOrEmpty(source))
+                    output.Append(".");
+
+                if (!string.IsNullOrEmpty(source))
+                {
+                    int sourceOffset = output.Length;
+                    output.Append(source);
+                    writerSegments[sourceIndex] = new Segment(sourceOffset, output.Length - sourceOffset);
+                }
+
+                output.Append("] ");
+            }
+
+            for (int i = 0; i != userProperties.Length; ++i)
+            {
+                if (i == 0 && !string.IsNullOrEmpty(text))
+                {
+                    bool endsWithPunctuation = char.IsPunctuation(text, text.Length - 1);
+                    if (endsWithPunctuation)
+                    {
+                        output.Append(" ");
+                        if (!string.IsNullOrEmpty(userProperties[i].Name))
+                        {
+                            output.Append(userProperties[i].Name);
+                            output.Append(": ");
+                        }
+
+                        int propertyOffset = output.Length;
+                        RenderValue(userProperties[i], sbf);
+                        userSegments[i] = new Segment(propertyOffset, output.Length - propertyOffset);
+                    }
+                    else if (string.IsNullOrEmpty(userProperties[i].Name))
+                    {
+                        output.Append(": ");
+                        int propertyOffset = output.Length;
+                        RenderValue(userProperties[i], sbf);
+                        userSegments[i] = new Segment(propertyOffset, output.Length - propertyOffset);
+                    }
+                    else
+                    {
+                        output.Append(". ");
+                        output.Append(userProperties[i].Name);
+                        output.Append(": ");
+                        int propertyOffset = output.Length;
+                        RenderValue(userProperties[i], sbf);
+                        userSegments[i] = new Segment(propertyOffset, output.Length - propertyOffset);
+                    }
+                }
+                else
+                {
+                    if (!string.IsNullOrEmpty(userProperties[i].Name))
+                    {
+                        output.Append(userProperties[i].Name);
+                        output.Append(": ");
+                    }
+
+                    int propertyOffset = output.Length;
+                    RenderValue(userProperties[i], sbf);
+                    userSegments[i] = new Segment(propertyOffset, output.Length - propertyOffset);
+                }
+
+                if (i + 1 < userProperties.Length)
+                {
+                    output.Append(", ");
+                }
+            }
+        }
+
+        private int FindDateTime(ReadOnlySpan<NamedProperty> properties, string name, out DateTime value)
+        {
+            for (int i = 0; i != properties.Length; ++i)
+            {
+                NamedProperty p = properties[i];
+
+                if (!string.Equals(p.Name, name, StringComparison.Ordinal))
+                    continue;
+
+                if (p.TryGetDateTime(out value))
+                    return i;
+            }
+
+            value = default;
+            return -1;
+        }
+
+        private int FindString(ReadOnlySpan<NamedProperty> properties, string name, out string value)
+        {
+            for (int i = 0; i != properties.Length; ++i)
+            {
+                NamedProperty p = properties[i];
+
+                if (!string.Equals(p.Name, name, StringComparison.Ordinal))
+                    continue;
+
+                if (p.TryGetString(out value))
+                    return i;
+            }
+
+            value = default;
+            return -1;
         }
 
         private static void RenderLevel(Level level, StringBuilder output)
@@ -49,6 +178,66 @@ namespace Phlogopite
                 default:
                     output.Append("-");
                     break;
+            }
+        }
+
+        private static void RenderTime(DateTime time, StringBuilderFacade sbf)
+        {
+            const string format = "HH:mm:ss.fff";
+            sbf.Append(time, format);
+        }
+
+        private void RenderValue(in NamedProperty p, StringBuilderFacade sbf)
+        {
+            switch (p.TypeCode)
+            {
+                case TypeCode.Empty:
+                    return;
+                case TypeCode.Boolean:
+                    sbf.Append(p.AsBoolean);
+                    return;
+                case TypeCode.Char:
+                    sbf.Append(p.AsChar);
+                    return;
+                case TypeCode.SByte:
+                    sbf.Append(p.AsSByte, "x2");
+                    return;
+                case TypeCode.Byte:
+                    sbf.Append(p.AsByte, "x2");
+                    return;
+                case TypeCode.Int16:
+                    sbf.Append(p.AsInt16);
+                    return;
+                case TypeCode.UInt16:
+                    sbf.Append(p.AsUInt16);
+                    return;
+                case TypeCode.Int32:
+                    sbf.Append(p.AsInt32);
+                    return;
+                case TypeCode.UInt32:
+                    sbf.Append(p.AsUInt32);
+                    return;
+                case TypeCode.Int64:
+                    sbf.Append(p.AsInt64);
+                    return;
+                case TypeCode.UInt64:
+                    sbf.Append(p.AsUInt64);
+                    return;
+                case TypeCode.Single:
+                    sbf.Append(p.AsSingle);
+                    return;
+                case TypeCode.Double:
+                    sbf.Append(p.AsDouble);
+                    return;
+                case TypeCode.DateTime:
+                    sbf.Append(p.AsDateTime);
+                    return;
+                case TypeCode.String:
+                    sbf.Append(p.AsString);
+                    return;
+                default:
+                    sbf.Append(p.AsObject);
+                    return;
             }
         }
     }
