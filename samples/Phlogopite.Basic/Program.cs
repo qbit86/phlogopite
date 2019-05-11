6145f537b75d1f550a2be36d41a516fe3417e2ca
Viktor T <qbit86@gmail.com>
2019-05-11 04:12:47 +0300

    Add Phlogopite.Basic

---
+++ samples/Phlogopite.Basic/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Phlogopite
+{
+    internal static class Program
+    {
+        private const string Tag = nameof(Program);
+
+        private static void Main()
+        {
+            var sinks = new[] { new ConsoleSink() };
+            var mediator = new Mediator(sinks);
+            var log = new Writer(mediator, Tag);
+            log.V("Hello, world!");
+        }
+    }
+}
