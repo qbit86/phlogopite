8ee6c91fd3744f755c560a3a66ae1d42a731ce31
Viktor T <qbit86@gmail.com>
2019-05-17 02:37:15 +0300

    Add stubs for Write method overloads

--- src/Phlogopite/Log.Write.cs
+++ src/Phlogopite/Log.Write.cs
@@ -1,6 +1,80 @@
+using System.Buffers;
+using System.Runtime.CompilerServices;
+
 namespace Phlogopite
 {
     public static partial class Log
     {
+        public static void Write(Level level, string tag, string text,
+            in NamedProperty p0,
+            [CallerMemberName] string source = null)
+        {
+            if (!Mediator.IsEnabled(level))
+                return;
+
+            NamedProperty[] properties = ArrayPool<NamedProperty>.Shared.Rent(3);
+            try
+            {
+                throw new System.NotImplementedException();
+            }
+            finally
+            {
+                ArrayPool<NamedProperty>.Shared.Return(properties, true);
+            }
+        }
+
+        public static void Write(Level level, string tag, string text,
+            in NamedProperty p0, in NamedProperty p1,
+            [CallerMemberName] string source = null)
+        {
+            if (!Mediator.IsEnabled(level))
+                return;
+
+            NamedProperty[] properties = ArrayPool<NamedProperty>.Shared.Rent(4);
+            try
+            {
+                throw new System.NotImplementedException();
+            }
+            finally
+            {
+                ArrayPool<NamedProperty>.Shared.Return(properties, true);
+            }
+        }
+
+        public static void Write(Level level, string tag, string text,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2,
+            [CallerMemberName] string source = null)
+        {
+            if (!Mediator.IsEnabled(level))
+                return;
+
+            NamedProperty[] properties = ArrayPool<NamedProperty>.Shared.Rent(5);
+            try
+            {
+                throw new System.NotImplementedException();
+            }
+            finally
+            {
+                ArrayPool<NamedProperty>.Shared.Return(properties, true);
+            }
+        }
+
+        public static void Write(Level level, string tag, string text,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            [CallerMemberName] string source = null)
+        {
+            if (!Mediator.IsEnabled(level))
+                return;
+
+            NamedProperty[] properties = ArrayPool<NamedProperty>.Shared.Rent(6);
+            try
+            {
+                throw new System.NotImplementedException();
+            }
+            finally
+            {
+                ArrayPool<NamedProperty>.Shared.Return(properties, true);
+            }
+        }
     }
 }
