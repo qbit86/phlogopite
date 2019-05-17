9f7eca64aa7f85e4a9eb25afd483eaee669fc494
Viktor T <qbit86@gmail.com>
2019-05-17 23:57:01 +0300

    Add overloads to WriterExtensions.Write() with no text

--- src/Phlogopite/Extensions/WriterExtensions.Write.cs
+++ src/Phlogopite/Extensions/WriterExtensions.Write.cs
@@ -2,6 +2,46 @@ namespace Phlogopite.Extensions
 {
     public static partial class WriterExtensions
     {
+        public static void Write<TWriter>(this TWriter writer, Level level,
+            in NamedProperty p0)
+            where TWriter : IWriter<NamedProperty>
+        {
+            if (writer is null || !writer.IsEnabled(level))
+                return;
+
+            WriteUnchecked(writer, level, null, p0);
+        }
+
+        public static void Write<TWriter>(this TWriter writer, Level level,
+            in NamedProperty p0, in NamedProperty p1)
+            where TWriter : IWriter<NamedProperty>
+        {
+            if (writer is null || !writer.IsEnabled(level))
+                return;
+
+            WriteUnchecked(writer, level, null, p0, p1);
+        }
+
+        public static void Write<TWriter>(this TWriter writer, Level level,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2)
+            where TWriter : IWriter<NamedProperty>
+        {
+            if (writer is null || !writer.IsEnabled(level))
+                return;
+
+            WriteUnchecked(writer, level, null, p0, p1, p2);
+        }
+
+        public static void Write<TWriter>(this TWriter writer, Level level,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3)
+            where TWriter : IWriter<NamedProperty>
+        {
+            if (writer is null || !writer.IsEnabled(level))
+                return;
+
+            WriteUnchecked(writer, level, null, p0, p1, p2, p3);
+        }
+
         public static void Write<TWriter>(this TWriter writer, Level level, string text,
             in NamedProperty p0)
             where TWriter : IWriter<NamedProperty>
