1eb16f587279cf87667bc45bb095a0662746e2d1
Viktor T <qbit86@gmail.com>
2019-05-17 03:20:43 +0300

    Add another Log.Write() overload

--- src/Phlogopite/Log.cs
+++ src/Phlogopite/Log.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Buffers;
+using System.Runtime.CompilerServices;
 
 namespace Phlogopite
 {
@@ -15,6 +17,26 @@ namespace Phlogopite
 
             s_mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
             return true;
+        }
+
+        public static void Write(Level level, string tag, string text,
+            [CallerMemberName] string source = null)
+        {
+            if (!Mediator.IsEnabled(level))
+                return;
+
+            NamedProperty[] properties = ArrayPool<NamedProperty>.Shared.Rent(2);
+            try
+            {
+                properties[0] = new NamedProperty("tag", tag);
+                properties[1] = new NamedProperty("source", source);
+                var writerProperties = new ReadOnlySpan<NamedProperty>(properties, 0, 2);
+                Mediator.Write(level, text, default, writerProperties);
+            }
+            finally
+            {
+                ArrayPool<NamedProperty>.Shared.Return(properties, true);
+            }
         }
     }
 }
