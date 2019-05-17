7db52feabd168611251f67a2afc9821589e24e81
Viktor T <qbit86@gmail.com>
2019-05-18 02:33:47 +0300

    Add method stubs

--- src/Phlogopite/Log.Level.cs
+++ src/Phlogopite/Log.Level.cs
@@ -1,6 +1,415 @@
+using System.Runtime.CompilerServices;
+
 namespace Phlogopite
 {
     public static partial class Log
     {
+        #region Verbose
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void V(string tag,
+            in NamedProperty p0,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void V(string tag,
+            in NamedProperty p0, in NamedProperty p1,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void V(string tag,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void V(string tag,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void V(string tag, string text,
+            in NamedProperty p0,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void V(string tag, string text,
+            in NamedProperty p0, in NamedProperty p1,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void V(string tag, string text,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void V(string tag, string text,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion // Verbose
+
+        #region Debug
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void D(string tag,
+            in NamedProperty p0,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void D(string tag,
+            in NamedProperty p0, in NamedProperty p1,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void D(string tag,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void D(string tag,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void D(string tag, string text,
+            in NamedProperty p0,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void D(string tag, string text,
+            in NamedProperty p0, in NamedProperty p1,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void D(string tag, string text,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void D(string tag, string text,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion // Debug
+
+        #region Info
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void I(string tag,
+            in NamedProperty p0,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void I(string tag,
+            in NamedProperty p0, in NamedProperty p1,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void I(string tag,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void I(string tag,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void I(string tag, string text,
+            in NamedProperty p0,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void I(string tag, string text,
+            in NamedProperty p0, in NamedProperty p1,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void I(string tag, string text,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void I(string tag, string text,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion // Info
+
+        #region Warning
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void W(string tag,
+            in NamedProperty p0,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void W(string tag,
+            in NamedProperty p0, in NamedProperty p1,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void W(string tag,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void W(string tag,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void W(string tag, string text,
+            in NamedProperty p0,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void W(string tag, string text,
+            in NamedProperty p0, in NamedProperty p1,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void W(string tag, string text,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void W(string tag, string text,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion // Warning
+
+        #region Error
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void E(string tag,
+            in NamedProperty p0,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void E(string tag,
+            in NamedProperty p0, in NamedProperty p1,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void E(string tag,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void E(string tag,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void E(string tag, string text,
+            in NamedProperty p0,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void E(string tag, string text,
+            in NamedProperty p0, in NamedProperty p1,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void E(string tag, string text,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void E(string tag, string text,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion // Error
+
+        #region Assert
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void A(string tag,
+            in NamedProperty p0,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void A(string tag,
+            in NamedProperty p0, in NamedProperty p1,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void A(string tag,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void A(string tag,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void A(string tag, string text,
+            in NamedProperty p0,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void A(string tag, string text,
+            in NamedProperty p0, in NamedProperty p1,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void A(string tag, string text,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void A(string tag, string text,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            [CallerMemberName] string source = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion // Assert
     }
 }
