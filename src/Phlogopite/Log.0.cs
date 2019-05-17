5f07522d7bfb8dc3af399d1367cb1699af4339b9
Viktor T <qbit86@gmail.com>
2019-05-17 09:48:50 +0300

    Add generating overloads for %Level% with no user properties

--- src/Phlogopite/Log.0.cs
+++ src/Phlogopite/Log.0.cs
@@ -1,6 +1,43 @@
+using System.Runtime.CompilerServices;
+
 namespace Phlogopite
 {
     public static partial class Log
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void V(string tag, string text, [CallerMemberName] string source = null)
+        {
+            Write(Level.Verbose, tag, text, source);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void D(string tag, string text, [CallerMemberName] string source = null)
+        {
+            Write(Level.Debug, tag, text, source);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void I(string tag, string text, [CallerMemberName] string source = null)
+        {
+            Write(Level.Info, tag, text, source);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void W(string tag, string text, [CallerMemberName] string source = null)
+        {
+            Write(Level.Warning, tag, text, source);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void E(string tag, string text, [CallerMemberName] string source = null)
+        {
+            Write(Level.Error, tag, text, source);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void A(string tag, string text, [CallerMemberName] string source = null)
+        {
+            Write(Level.Assert, tag, text, source);
+        }
     }
 }
