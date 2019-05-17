a2186145df58a947294ff5e84254f28ce214f17d
Viktor T <qbit86@gmail.com>
2019-05-17 09:51:11 +0300

    Add aggressive inlining for some code

--- src/Phlogopite/Extensions/WriterExtensions.0.cs
+++ src/Phlogopite/Extensions/WriterExtensions.0.cs
@@ -1,37 +1,45 @@
+using System.Runtime.CompilerServices;
+
 namespace Phlogopite.Extensions
 {
     public static partial class WriterExtensions
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void V<TWriter>(this TWriter writer, string text)
             where TWriter : IWriter<NamedProperty>
         {
             writer.Write(Level.Verbose, text, default);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void D<TWriter>(this TWriter writer, string text)
             where TWriter : IWriter<NamedProperty>
         {
             writer.Write(Level.Debug, text, default);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void I<TWriter>(this TWriter writer, string text)
             where TWriter : IWriter<NamedProperty>
         {
             writer.Write(Level.Info, text, default);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void W<TWriter>(this TWriter writer, string text)
             where TWriter : IWriter<NamedProperty>
         {
             writer.Write(Level.Warning, text, default);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void E<TWriter>(this TWriter writer, string text)
             where TWriter : IWriter<NamedProperty>
         {
             writer.Write(Level.Error, text, default);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void A<TWriter>(this TWriter writer, string text)
             where TWriter : IWriter<NamedProperty>
         {
