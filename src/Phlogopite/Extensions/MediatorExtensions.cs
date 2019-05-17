63d77dcfd3b61f89ed24aeae045afc7cae46c2dd
Viktor T <qbit86@gmail.com>
2019-05-18 00:47:55 +0300

    Add MediatorExtensions

---
+++ src/Phlogopite/Extensions/MediatorExtensions.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Buffers;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Phlogopite.Extensions
+{
+    public static class MediatorExtensions
+    {
+        internal static void WriteUnchecked(IMediator<NamedProperty> mediator, Level level, string tag, string text,
+            [CallerMemberName] string source = null)
+        {
+            Debug.Assert(mediator != null, "mediator != null");
+
+            NamedProperty[] properties = ArrayPool<NamedProperty>.Shared.Rent(2);
+            try
+            {
+                properties[0] = new NamedProperty("tag", tag);
+                properties[1] = new NamedProperty("source", source);
+                var writerProperties = new ReadOnlySpan<NamedProperty>(properties, 0, 2);
+                mediator.UncheckedWrite(level, text, default, writerProperties);
+            }
+            finally
+            {
+                ArrayPool<NamedProperty>.Shared.Return(properties, true);
+            }
+        }
+    }
+}
