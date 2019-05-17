e085a9a8b9fc33bfafba590342561af497b88433
Viktor T <qbit86@gmail.com>
2019-05-17 21:35:36 +0300

    Moved generating WriteUnchecked() to separate template

--- src/Phlogopite/Extensions/WriterExtensions.Write.cs
+++ src/Phlogopite/Extensions/WriterExtensions.Write.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Buffers;
-
 namespace Phlogopite.Extensions
 {
     public static partial class WriterExtensions
@@ -43,76 +40,6 @@ namespace Phlogopite.Extensions
                 return;
 
             WriteUnchecked(writer, level, text, p0, p1, p2, p3);
-        }
-
-        private static void WriteUnchecked<TWriter, TProperty>(in TWriter writer, Level level, string text,
-            in TProperty p0)
-            where TWriter : IWriter<TProperty>
-        {
-            TProperty[] properties = ArrayPool<TProperty>.Shared.Rent(1);
-            try
-            {
-                properties[0] = p0;
-                writer.Write(level, text, properties.AsSpan(0, 1));
-            }
-            finally
-            {
-                ArrayPool<TProperty>.Shared.Return(properties);
-            }
-        }
-
-        private static void WriteUnchecked<TWriter, TProperty>(in TWriter writer, Level level, string text,
-            in TProperty p0, in TProperty p1)
-            where TWriter : IWriter<TProperty>
-        {
-            TProperty[] properties = ArrayPool<TProperty>.Shared.Rent(2);
-            try
-            {
-                properties[0] = p0;
-                properties[1] = p1;
-                writer.Write(level, text, properties.AsSpan(0, 2));
-            }
-            finally
-            {
-                ArrayPool<TProperty>.Shared.Return(properties);
-            }
-        }
-
-        private static void WriteUnchecked<TWriter, TProperty>(in TWriter writer, Level level, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2)
-            where TWriter : IWriter<TProperty>
-        {
-            TProperty[] properties = ArrayPool<TProperty>.Shared.Rent(3);
-            try
-            {
-                properties[0] = p0;
-                properties[1] = p1;
-                properties[2] = p2;
-                writer.Write(level, text, properties.AsSpan(0, 3));
-            }
-            finally
-            {
-                ArrayPool<TProperty>.Shared.Return(properties);
-            }
-        }
-
-        private static void WriteUnchecked<TWriter, TProperty>(in TWriter writer, Level level, string text,
-            in TProperty p0, in TProperty p1, in TProperty p2, in TProperty p3)
-            where TWriter : IWriter<TProperty>
-        {
-            TProperty[] properties = ArrayPool<TProperty>.Shared.Rent(4);
-            try
-            {
-                properties[0] = p0;
-                properties[1] = p1;
-                properties[2] = p2;
-                properties[3] = p3;
-                writer.Write(level, text, properties.AsSpan(0, 4));
-            }
-            finally
-            {
-                ArrayPool<TProperty>.Shared.Return(properties);
-            }
         }
     }
 }
