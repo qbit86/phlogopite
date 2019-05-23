37ab0742532c49bd66f49182fef0b5ad6022fb4a
Viktor T <qbit86@gmail.com>
2019-05-24 02:31:31 +0300

    Refactor WriterExtensions

--- src/Phlogopite/Extensions/WriterExtensions.cs
+++ src/Phlogopite/Extensions/WriterExtensions.cs
@@ -4,6 +4,8 @@ namespace Phlogopite.Extensions
 {
     public static partial class WriterExtensions
     {
+        private const int WriterPropertyCount = 2;
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Write<TWriter>(this TWriter writer, Level level, string text)
             where TWriter : IWriter<NamedProperty>
