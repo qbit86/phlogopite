7eb76d2b764a8cffc72fd221b88c8acbdae0e914
Viktor T <qbit86@gmail.com>
2019-05-30 01:46:44 +0300

    Remove allocating in Mediator.UncheckedWrite

--- src/Phlogopite.Abstractions/IMediator.cs
+++ src/Phlogopite.Abstractions/IMediator.cs
@@ -4,6 +4,8 @@ namespace Phlogopite
 {
     public interface IMediator<TProperty>
     {
+        int GetAttachedPropertyCount(Level level);
+
         bool IsEnabled(Level level);
 
         void UncheckedWrite(Level level, string text, ReadOnlySpan<TProperty> userProperties,
