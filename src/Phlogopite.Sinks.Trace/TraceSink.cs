829d66ea6ce4586e25b1a845ad01406fc018ef6b
Viktor T <qbit86@gmail.com>
2019-06-09 12:28:00 +0300

    Add TraceSink stub

---
+++ src/Phlogopite.Sinks.Trace/TraceSink.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Phlogopite.Sinks
+{
+    public sealed class TraceSink : ISink<NamedProperty>, IFormattedSink<NamedProperty>
+    {
+        public bool IsEnabled(Level level)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UncheckedWrite(Level level, string text, ReadOnlySpan<NamedProperty> userProperties,
+            ReadOnlySpan<NamedProperty> writerProperties, ReadOnlySpan<NamedProperty> mediatorProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UncheckedWrite(Level level, string text, ReadOnlySpan<NamedProperty> userProperties,
+            ReadOnlySpan<NamedProperty> writerProperties, ReadOnlySpan<NamedProperty> mediatorProperties,
+            ArraySegment<char> formattedMessage,
+            ReadOnlySpan<Range> userRanges, ReadOnlySpan<Range> writerRanges, ReadOnlySpan<Range> mediatorRanges)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
