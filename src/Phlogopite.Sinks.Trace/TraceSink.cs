beed5e8690186329035172ff99f72ef8360f0ad1
Viktor T <qbit86@gmail.com>
2019-06-09 12:34:36 +0300

    Add _minimumLevel

--- src/Phlogopite.Sinks.Trace/TraceSink.cs
+++ src/Phlogopite.Sinks.Trace/TraceSink.cs
@@ -4,21 +4,32 @@ namespace Phlogopite.Sinks
 {
     public sealed class TraceSink : ISink<NamedProperty>, IFormattedSink<NamedProperty>
     {
-        public bool IsEnabled(Level level)
-        {
-            throw new NotImplementedException();
-        }
+        internal const Level DefaultMinimumLevel = Level.Verbose;
 
-        public void UncheckedWrite(Level level, string text, ReadOnlySpan<NamedProperty> userProperties,
-            ReadOnlySpan<NamedProperty> writerProperties, ReadOnlySpan<NamedProperty> mediatorProperties)
+        private readonly Level _minimumLevel;
+
+        public TraceSink() : this(DefaultMinimumLevel) { }
+
+        public TraceSink(Level minimumLevel)
         {
-            throw new NotImplementedException();
+            _minimumLevel = minimumLevel;
         }
 
         public void UncheckedWrite(Level level, string text, ReadOnlySpan<NamedProperty> userProperties,
             ReadOnlySpan<NamedProperty> writerProperties, ReadOnlySpan<NamedProperty> mediatorProperties,
             ArraySegment<char> formattedMessage,
             ReadOnlySpan<Range> userRanges, ReadOnlySpan<Range> writerRanges, ReadOnlySpan<Range> mediatorRanges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEnabled(Level level)
+        {
+            return _minimumLevel <= level;
+        }
+
+        public void UncheckedWrite(Level level, string text, ReadOnlySpan<NamedProperty> userProperties,
+            ReadOnlySpan<NamedProperty> writerProperties, ReadOnlySpan<NamedProperty> mediatorProperties)
         {
             throw new NotImplementedException();
         }
