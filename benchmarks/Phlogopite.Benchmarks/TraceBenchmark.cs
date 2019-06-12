ab181dea95a78935dd84dde85bc8ff14a2d9f893
Viktor T <qbit86@gmail.com>
2019-06-12 10:48:20 +0300

    Add logging to Extensions

--- benchmarks/Phlogopite.Benchmarks/TraceBenchmark.cs
+++ benchmarks/Phlogopite.Benchmarks/TraceBenchmark.cs
@@ -1,11 +1,15 @@
 using System;
+using System.Diagnostics;
 using BenchmarkDotNet.Attributes;
+using Microsoft.Extensions.Logging;
 using Serilog;
+using Serilog.Events;
 
 namespace Phlogopite
 {
     public abstract class TraceBenchmark
     {
+        private Microsoft.Extensions.Logging.ILogger _extensionsLogger;
         private Serilog.Core.Logger _serilogLogger;
         private DateTime _now;
         private string _username;
@@ -17,13 +21,37 @@ namespace Phlogopite
             _username = Environment.UserName;
         }
 
+        [GlobalSetup(Target = nameof(TraceToExtensions))]
+        public void GlobalSetupExtensions()
+        {
+            ILoggerFactory loggerFactory = LoggerFactory.Create(Configure);
+            using (loggerFactory)
+            {
+                _extensionsLogger = loggerFactory.CreateLogger(nameof(TraceBenchmark));
+            }
+
+            void Configure(ILoggingBuilder builder)
+            {
+                var sourceSwitch = new SourceSwitch(nameof(TraceBenchmark)) { Level = SourceLevels.All };
+                builder.AddTraceSource(sourceSwitch);
+            }
+        }
+
         [GlobalSetup(Target = nameof(TraceToSerilog))]
         public void GlobalSetupSerilog()
         {
             _serilogLogger = new LoggerConfiguration()
-                .WriteTo.Trace(Serilog.Events.LogEventLevel.Verbose,
+                .WriteTo.Trace(LogEventLevel.Verbose,
                     $"{{Level:u1}} {{Timestamp:HH:mm:ss.fff}} [{nameof(TraceBenchmark)}] {{Message}}")
                 .CreateLogger();
+        }
+
+        [Benchmark]
+        public string TraceToExtensions()
+        {
+            _extensionsLogger.LogInformation("Hello, world! now: {now}, e: {e}, username: {username}",
+                _now, Math.E, _username);
+            return _username;
         }
 
         [Benchmark(Baseline = true)]
@@ -38,12 +66,6 @@ namespace Phlogopite
             _serilogLogger.Information("Hello, world! now: {now}, e: {e}, username: {username}",
                 _now, Math.E, _username);
             return _username;
-        }
-
-        [Benchmark]
-        public string TraceToExtensions()
-        {
-            throw new NotImplementedException();
         }
     }
 }
