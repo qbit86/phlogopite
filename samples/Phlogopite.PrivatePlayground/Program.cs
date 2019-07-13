9633516d17e149d70fbc0630ae6e5cd399eefcd9
Viktor T <qbit86@gmail.com>
2019-07-13 11:41:54 +0300

    Clean samples

--- samples/Phlogopite.PrivatePlayground/Program.cs
+++ samples/Phlogopite.PrivatePlayground/Program.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Globalization;
 using System.Threading;
-using Phlogopite.Extensions;
-using Phlogopite.Sinks;
 
 namespace Phlogopite
 {
@@ -13,13 +10,6 @@ namespace Phlogopite
             // Messing with culture.
             Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("ru-RU");
 
-            ConsoleSink consoleSink = new ConsoleSinkBuilder { EmitLevel = true, EmitTime = true }.Build();
-
-            Mediator mediator = new MediatorBuilder { MinimumLevel = Level.Debug }
-                .AddSink(consoleSink)
-                .Build();
-            _ = Log.TrySetMediator(mediator);
-
             var foo = new Foo();
             foo.Bar();
         }
@@ -27,18 +17,8 @@ namespace Phlogopite
 
     internal sealed class Foo
     {
-        private readonly WriterBuilder _log;
-
-        internal Foo(IMediator<NamedProperty> mediator = null)
-        {
-            _log = new WriterBuilder(mediator ?? Log.Mediator, Level.Info, nameof(Foo));
-        }
-
         internal void Bar()
         {
-            _log.V("This should be disabled by mediator");
-            _log.D("This should be disabled by writer");
-            _log.I("Testing things", ("username", Environment.UserName), (nameof(Math.E), Math.E));
         }
     }
 }
