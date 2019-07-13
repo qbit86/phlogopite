626d1e3f2d0c6afc4058c43b39f2794cca56b834
Viktor T <qbit86@gmail.com>
2019-07-13 13:09:49 +0300

    Add AggregateLogger<TProperty, TProperties>

---
+++ src/Phlogopite/AggregateLogger_2.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Phlogopite
+{
+    public sealed class AggregateLogger<TProperty, TProperties> : ILogger<TProperty, TProperties>
+    {
+        internal const Level DefaultMinimumLevel = Level.Verbose;
+
+        private readonly Func<Exception, bool> _exceptionHandler;
+        private readonly IReadOnlyList<ILogger<TProperty, TProperties>> _loggers;
+        private readonly Level _minimumLevel;
+        private readonly Func<Level> _minimumLevelProvider;
+
+        internal AggregateLogger(IReadOnlyList<ILogger<TProperty, TProperties>> loggers, int maxAttachedPropertyCount,
+            Level minimumLevel, Func<Level> minimumLevelProvider, Func<Exception, bool> exceptionHandler)
+        {
+            Debug.Assert(loggers != null, "loggers != null");
+            _exceptionHandler = exceptionHandler;
+            _loggers = loggers;
+            _minimumLevel = minimumLevel;
+            _minimumLevelProvider = minimumLevelProvider;
+            MaxAttachedPropertyCount = maxAttachedPropertyCount;
+        }
+
+        public int MaxAttachedPropertyCount { get; }
+
+        public bool IsEnabled(Level level)
+        {
+            Level minimumLevel = _minimumLevelProvider?.Invoke() ?? _minimumLevel;
+            if (minimumLevel > level)
+                return false;
+
+            return _loggers.Count != 0;
+        }
+
+        public void UncheckedWrite(Level level, string text, TProperties attachedProperties,
+            ReadOnlySpan<TProperty> userProperties)
+        {
+            List<Exception> exceptions = null;
+            int count = _loggers.Count;
+            for (int i = 0; i < count; ++i)
+            {
+                try
+                {
+                    ILogger<TProperty, TProperties> logger = _loggers[i];
+                    if (logger is null || !logger.IsEnabled(level))
+                        continue;
+
+                    logger.UncheckedWrite(level, text, attachedProperties, userProperties);
+                }
+#pragma warning disable CA1031 // Do not catch general exception types
+                catch (Exception ex)
+                {
+                    if (exceptions is null)
+                        exceptions = new List<Exception>();
+
+                    exceptions.Add(ex);
+                }
+#pragma warning restore CA1031 // Do not catch general exception types
+            }
+
+            if (exceptions is null)
+                return;
+
+            var aggregateException = new AggregateException(exceptions);
+            if (_exceptionHandler is null)
+                throw aggregateException;
+
+            aggregateException.Handle(_exceptionHandler);
+        }
+    }
+}
