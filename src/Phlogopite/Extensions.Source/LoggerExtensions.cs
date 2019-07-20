e2056e5ace23a7ffb0a20feec36ceda7664973a3
Viktor T <qbit86@gmail.com>
2019-07-21 02:37:50 +0300

    Add Extensions.Source/LoggerExtensions

---
+++ src/Phlogopite/Extensions.Source/LoggerExtensions.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Buffers;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Phlogopite.Extensions.Source
+{
+    public static partial class LoggerExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Write<TLogger>(this TLogger logger, Level level, string text,
+            [CallerMemberName] string source = null)
+            where TLogger : ILogger<NamedProperty, ArraySegment<NamedProperty>>
+        {
+            if (logger is null || !logger.IsEnabled(Level.Error))
+                return;
+
+            AllocateThenWrite0(logger, level, text, source);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int GetAttachedPropertyCountOrDefault<TLogger>(TLogger logger)
+            where TLogger : ILogger<NamedProperty, ArraySegment<NamedProperty>>
+        {
+            Debug.Assert(logger != null, "logger != null");
+
+            return Math.Max(0, logger.MaxAttachedPropertyCount);
+        }
+
+        private static void AllocateThenWrite0<TLogger>(TLogger logger, Level level, string text,
+            string source)
+            where TLogger : ILogger<NamedProperty, ArraySegment<NamedProperty>>
+        {
+            Debug.Assert(logger != null, "logger != null");
+            Debug.Assert(logger.IsEnabled(level), "logger.IsEnabled(level)");
+
+            const int userPropertyCount = 0;
+            int attachedPropertyCount = GetAttachedPropertyCountOrDefault(logger);
+            NamedProperty[] properties = ArrayPool<NamedProperty>.Shared.Rent(
+                userPropertyCount + attachedPropertyCount + 1);
+            try
+            {
+                Span<NamedProperty> userProperties = properties.AsSpan(0, userPropertyCount);
+                var attachedProperties = new ArraySegment<NamedProperty>(properties, userPropertyCount, 0);
+                AppendThenWrite(logger, level, text, attachedProperties, userProperties, source);
+            }
+            finally
+            {
+                ArrayPool<NamedProperty>.Shared.Return(properties, true);
+            }
+        }
+
+        private static void AppendThenWrite<TLogger>(TLogger logger, Level level, string text,
+            ArraySegment<NamedProperty> attachedProperties, ReadOnlySpan<NamedProperty> userProperties, string source)
+            where TLogger : ILogger<NamedProperty, ArraySegment<NamedProperty>>
+        {
+            Debug.Assert(logger != null, "logger != null");
+            Debug.Assert(logger.IsEnabled(level), "logger.IsEnabled(level)");
+
+            if (source != null)
+                PropertyHelpers.TryAdd(ref attachedProperties, new NamedProperty(KnownProperties.Source, source));
+
+            logger.UncheckedWrite(level, text, attachedProperties, userProperties);
+        }
+    }
+}
