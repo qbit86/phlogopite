32904e5dc9abc0e76f980f4c54b1a6f200ef4faf
Viktor T <qbit86@gmail.com>
2019-07-20 13:33:33 +0300

    Implement Write<>() methods (inckuding text)

--- src/Phlogopite/Extensions.Category/CategoryExtensions.Write.cs
+++ src/Phlogopite/Extensions.Category/CategoryExtensions.Write.cs
@@ -59,6 +59,54 @@ namespace Phlogopite.Extensions.Category
 
         #region Including text
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Write<TLogger>(this TLogger logger, Level level, string category, string text,
+            in NamedProperty p0,
+            [CallerMemberName] string source = null)
+            where TLogger : ILogger<NamedProperty, ArraySegment<NamedProperty>>
+        {
+            if (logger is null || !logger.IsEnabled(Level.Error))
+                return;
+
+            UncheckedWrite1(logger, level, category, text, p0, source);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Write<TLogger>(this TLogger logger, Level level, string category, string text,
+            in NamedProperty p0, in NamedProperty p1,
+            [CallerMemberName] string source = null)
+            where TLogger : ILogger<NamedProperty, ArraySegment<NamedProperty>>
+        {
+            if (logger is null || !logger.IsEnabled(Level.Error))
+                return;
+
+            UncheckedWrite2(logger, level, category, text, p0, p1, source);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Write<TLogger>(this TLogger logger, Level level, string category, string text,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2,
+            [CallerMemberName] string source = null)
+            where TLogger : ILogger<NamedProperty, ArraySegment<NamedProperty>>
+        {
+            if (logger is null || !logger.IsEnabled(Level.Error))
+                return;
+
+            UncheckedWrite3(logger, level, category, text, p0, p1, p2, source);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Write<TLogger>(this TLogger logger, Level level, string category, string text,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            [CallerMemberName] string source = null)
+            where TLogger : ILogger<NamedProperty, ArraySegment<NamedProperty>>
+        {
+            if (logger is null || !logger.IsEnabled(Level.Error))
+                return;
+
+            UncheckedWrite4(logger, level, category, text, p0, p1, p2, p3, source);
+        }
+
         #endregion Including text
     }
 }
