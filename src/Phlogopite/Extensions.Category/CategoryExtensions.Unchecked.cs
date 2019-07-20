339080a09a0c8ace8ce2400de848d4afca4d2839
Viktor T <qbit86@gmail.com>
2019-07-20 12:38:14 +0300

    Add CategoryExtensions.Unchecked stub

--- src/Phlogopite/Extensions.Category/CategoryExtensions.Unchecked.cs
+++ src/Phlogopite/Extensions.Category/CategoryExtensions.Unchecked.cs
@@ -4,4 +4,38 @@ using System.Diagnostics;
 
 namespace Phlogopite.Extensions.Category
 {
+    public static partial class CategoryExtensions
+    {
+        private static void UncheckedWrite<TLogger>(TLogger logger, Level level, string text,
+            in NamedProperty p0,
+            string source)
+            where TLogger : ILogger<NamedProperty, ArraySegment<NamedProperty>>
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void UncheckedWrite<TLogger>(TLogger logger, Level level, string text,
+            in NamedProperty p0, in NamedProperty p1,
+            string source)
+            where TLogger : ILogger<NamedProperty, ArraySegment<NamedProperty>>
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void UncheckedWrite<TLogger>(TLogger logger, Level level, string text,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2,
+            string source)
+            where TLogger : ILogger<NamedProperty, ArraySegment<NamedProperty>>
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void UncheckedWrite<TLogger>(TLogger logger, Level level, string text,
+            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
+            string source)
+            where TLogger : ILogger<NamedProperty, ArraySegment<NamedProperty>>
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
