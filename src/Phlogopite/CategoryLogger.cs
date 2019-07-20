f4267df8cd79a2d8cf761f0032c264a30233cef4
Viktor T <qbit86@gmail.com>
2019-07-20 12:23:18 +0300

    Refactor CategoryLogger

--- src/Phlogopite/CategoryLogger.cs
+++ src/Phlogopite/CategoryLogger.cs
@@ -31,19 +31,10 @@ namespace Phlogopite
         public void UncheckedWrite(Level level, string text, ArraySegment<NamedProperty> attachedProperties,
             ReadOnlySpan<NamedProperty> userProperties)
         {
-            NamedProperty[] array = attachedProperties.Array;
-            int offset = attachedProperties.Offset;
-            int oldCount = attachedProperties.Count;
+            if (_category != null)
+                PropertyHelpers.TryAdd(ref attachedProperties, new NamedProperty(KnownProperties.Category, _category));
 
-            if (array is null || offset + oldCount >= array.Length)
-            {
-                _logger.UncheckedWrite(level, text, attachedProperties, userProperties);
-                return;
-            }
-
-            var newAttachedProperties = new ArraySegment<NamedProperty>(array, offset, oldCount + 1);
-            array[offset + oldCount] = new NamedProperty(KnownProperties.Category, _category);
-            _logger.UncheckedWrite(level, text, newAttachedProperties, userProperties);
+            _logger.UncheckedWrite(level, text, attachedProperties, userProperties);
         }
 
         public bool IsEnabled(Level level)
