7a3ae5aad39d7ae58801daa6762b8247fa5f5aff
Viktor T <qbit86@gmail.com>
2019-07-21 22:01:43 +0300

    Remove overloads omitting text

--- src/Phlogopite/Extensions.Tag/TagLoggerExtensions.Write.cs
+++ src/Phlogopite/Extensions.Tag/TagLoggerExtensions.Write.cs
@@ -5,60 +5,6 @@ namespace Phlogopite.Extensions.Tag
 {
     public static partial class TagLoggerExtensions
     {
-        #region Omitting text
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void Write<TLogger>(this TLogger logger, Level level, string category,
-            in NamedProperty p0,
-            [CallerMemberName] string source = null)
-            where TLogger : ILogger<NamedProperty, ArraySegment<NamedProperty>>
-        {
-            if (logger is null || !logger.IsEnabled(level))
-                return;
-
-            AllocateThenWrite1(logger, level, category, null, p0, source);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void Write<TLogger>(this TLogger logger, Level level, string category,
-            in NamedProperty p0, in NamedProperty p1,
-            [CallerMemberName] string source = null)
-            where TLogger : ILogger<NamedProperty, ArraySegment<NamedProperty>>
-        {
-            if (logger is null || !logger.IsEnabled(level))
-                return;
-
-            AllocateThenWrite2(logger, level, category, null, p0, p1, source);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void Write<TLogger>(this TLogger logger, Level level, string category,
-            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2,
-            [CallerMemberName] string source = null)
-            where TLogger : ILogger<NamedProperty, ArraySegment<NamedProperty>>
-        {
-            if (logger is null || !logger.IsEnabled(level))
-                return;
-
-            AllocateThenWrite3(logger, level, category, null, p0, p1, p2, source);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void Write<TLogger>(this TLogger logger, Level level, string category,
-            in NamedProperty p0, in NamedProperty p1, in NamedProperty p2, in NamedProperty p3,
-            [CallerMemberName] string source = null)
-            where TLogger : ILogger<NamedProperty, ArraySegment<NamedProperty>>
-        {
-            if (logger is null || !logger.IsEnabled(level))
-                return;
-
-            AllocateThenWrite4(logger, level, category, null, p0, p1, p2, p3, source);
-        }
-
-        #endregion Omitting text
-
-        #region Including text
-
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Write<TLogger>(this TLogger logger, Level level, string category, string text,
             in NamedProperty p0,
@@ -106,7 +52,5 @@ namespace Phlogopite.Extensions.Tag
 
             AllocateThenWrite4(logger, level, category, text, p0, p1, p2, p3, source);
         }
-
-        #endregion Including text
     }
 }
