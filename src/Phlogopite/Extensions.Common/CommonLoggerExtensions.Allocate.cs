c8e8a4438376525ed423c1bc6bb92d3fcb6de5fb
Viktor T <qbit86@gmail.com>
2019-07-25 02:12:04 +0300

    Add CommonLoggerExtensions stub

---
+++ src/Phlogopite/Extensions.Common/CommonLoggerExtensions.Allocate.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Buffers;
+using System.Diagnostics;
+using PropertyCollection = Phlogopite.SpanBuilder<Phlogopite.NamedProperty>;
+
+namespace Phlogopite.Extensions.Common
+{
+    public static partial class CommonLoggerExtensions
+    {
+    }
+}
---
+++ src/Phlogopite/Extensions.Common/CommonLoggerExtensions.Level.cs
@@ -0,0 +1,9 @@
+using System.Runtime.CompilerServices;
+using PropertyCollection = Phlogopite.SpanBuilder<Phlogopite.NamedProperty>;
+
+namespace Phlogopite.Extensions.Common
+{
+    public static partial class CommonLoggerExtensions
+    {
+    }
+}
