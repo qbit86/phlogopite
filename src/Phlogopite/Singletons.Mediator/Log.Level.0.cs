f2588869d8bb97e45fe6c481d1f88e6d0d20b153
Viktor T <qbit86@gmail.com>
2019-07-26 02:31:28 +0300

    Implement Log.Level.0.tt

---
+++ src/Phlogopite/Singletons.Mediator/Log.Level.0.cs
@@ -0,0 +1,51 @@
+using System.Runtime.CompilerServices;
+using Phlogopite.Extensions.Tag;
+using PropertyCollection = Phlogopite.SpanBuilder<Phlogopite.NamedProperty>;
+
+namespace Phlogopite.Singletons.Mediator
+{
+    public static partial class Log
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void V(string category, string text,
+            [CallerMemberName] string source = null)
+        {
+            Logger.Write(Level.Verbose, category, text, source);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void D(string category, string text,
+            [CallerMemberName] string source = null)
+        {
+            Logger.Write(Level.Debug, category, text, source);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void I(string category, string text,
+            [CallerMemberName] string source = null)
+        {
+            Logger.Write(Level.Info, category, text, source);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void W(string category, string text,
+            [CallerMemberName] string source = null)
+        {
+            Logger.Write(Level.Warning, category, text, source);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void E(string category, string text,
+            [CallerMemberName] string source = null)
+        {
+            Logger.Write(Level.Error, category, text, source);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void A(string category, string text,
+            [CallerMemberName] string source = null)
+        {
+            Logger.Write(Level.Assert, category, text, source);
+        }
+    }
+}
