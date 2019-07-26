5e3be679fe8ffcff3eeea4edc035c1dc9ad27faf
Viktor T <qbit86@gmail.com>
2019-07-27 01:31:30 +0300

    Move files to Abstractions

--- src/Phlogopite/CollectionHelpers.cs
+++ src/Phlogopite/CollectionHelpers.cs
@@ -4,16 +4,7 @@ namespace Phlogopite
     {
         internal static bool TryAppend<T>(ref SpanBuilder<T> collection, T item)
         {
-            T[] array = collection.Array;
-            int offset = collection.Offset;
-            int oldCount = collection.Count;
-
-            if (array is null || offset + oldCount >= array.Length)
-                return false;
-
-            array[offset + oldCount] = item;
-            collection = new SpanBuilder<T>(array, offset, oldCount + 1);
-            return true;
+            return collection.TryAppend(item, out collection);
         }
     }
 }
