b6df0bfc175fc175ed7e23ae9c43be95cebc790e
Viktor T <qbit86@gmail.com>
2019-07-26 23:37:50 +0300

    Add «Lazy Cat»

--- samples/Phlogopite.Demo.Basic/Program.cs
+++ samples/Phlogopite.Demo.Basic/Program.cs
@@ -1,7 +1,36 @@
-﻿namespace Phlogopite
+using System;
+using Phlogopite.Extensions.Source;
+using Phlogopite.Singletons.Logger;
+using Cat =
+    Phlogopite.CategoryLogger<
+        Phlogopite.ILogger<Phlogopite.NamedProperty, Phlogopite.SpanBuilder<Phlogopite.NamedProperty>>>;
+
+#pragma warning disable CA1303 // Do not pass literals as localized parameters
+
+namespace Phlogopite
 {
     internal static class Program
     {
-        private static void Main() { }
+        private static void Main()
+        {
+            var logger = new ConsoleLogger(Level.Debug);
+            Log.TrySetLogger(logger);
+
+            var foo = new Foo();
+            foo.Bar();
+        }
+    }
+
+    internal sealed class Foo
+    {
+        private static readonly Lazy<Cat> s_logger =
+            new Lazy<Cat>(() => CategoryLogger.Create(Log.Logger, nameof(Foo)));
+
+        private static Cat L => s_logger.Value;
+
+        internal void Bar()
+        {
+            L.I("Hello!", (nameof(Environment.UserName), Environment.UserName));
+        }
     }
 }
