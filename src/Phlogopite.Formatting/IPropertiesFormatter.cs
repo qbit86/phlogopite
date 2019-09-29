92b350535f397fe2c69dc388ca98722d29de3cd1
Viktor T <qbit86@gmail.com>
2019-09-30 01:08:08 +0300

    Add PropertiesFormatter stub

---
+++ src/Phlogopite.Formatting/IPropertiesFormatter.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Text;
+
+// ReSharper disable once CheckNamespace
+
+namespace Phlogopite
+{
+    public interface IPropertiesFormatter<TProperty>
+    {
+        void Format(ReadOnlySpan<TProperty> userProperties, ReadOnlySpan<TProperty> attachedProperties,
+            StringBuilder output, Span<Range> userRanges, Span<Range> attachedRanges, IFormatProvider formatProvider);
+    }
+}
---
+++ src/Phlogopite.Formatting/PropertiesFormatter.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Text;
+
+namespace Phlogopite
+{
+    public class PropertiesFormatter : IPropertiesFormatter<NamedProperty>
+    {
+        private PropertiesFormatter() { }
+
+        public static PropertiesFormatter Default { get; } = new PropertiesFormatter();
+
+        public void Format(ReadOnlySpan<NamedProperty> userProperties, ReadOnlySpan<NamedProperty> attachedProperties,
+            StringBuilder output, Span<Range> userRanges, Span<Range> attachedRanges, IFormatProvider formatProvider)
+        {
+            if (output is null)
+                throw new ArgumentNullException(nameof(output));
+
+            if (formatProvider is null)
+                throw new ArgumentNullException(nameof(formatProvider));
+
+            FormatUserProperties(userProperties, output, userRanges, formatProvider);
+            FormatAttachedProperties(attachedProperties, output, attachedRanges, formatProvider);
+        }
+
+        private void FormatUserProperties(ReadOnlySpan<NamedProperty> userProperties,
+            StringBuilder output, Span<Range> userRanges, IFormatProvider formatProvider)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void FormatAttachedProperties(ReadOnlySpan<NamedProperty> attachedProperties,
+            StringBuilder output, Span<Range> attachedRanges, IFormatProvider formatProvider)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void SetRange(int rangeIndex, int propertyOffset, StringBuilder sb, Span<Range> ranges)
+        {
+            if ((uint)rangeIndex >= (uint)ranges.Length)
+                return;
+
+            ranges[rangeIndex] = new Range(propertyOffset, sb.Length);
+        }
+    }
+}
