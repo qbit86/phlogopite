c33f557e8b2b7b81cce6471ec756b3dc56c80222
Viktor T <qbit86@gmail.com>
2020-03-21 02:54:36 +0300

    Refactor SpanBuilder to operate on span instead of array

--- src/Phlogopite.Abstractions/SR.cs
+++ src/Phlogopite.Abstractions/SR.cs
@@ -5,7 +5,7 @@ namespace Phlogopite
     internal static class SR
     {
         internal const string Argument_InvalidOffLen =
-            "Offset and length were out of bounds for the array or count is greater than the number of elements from index to the end of the source collection.";
+            "Offset and length were out of bounds or count is greater than the number of elements from index to the end of the source collection.";
 
         internal const string ArgumentOutOfRange_NeedNonNegNum = "Non-negative number required.";
     }
--- src/Phlogopite.Abstractions/SpanBuilder.cs
+++ src/Phlogopite.Abstractions/SpanBuilder.cs
@@ -1,18 +1,17 @@
 using System;
-using System.Diagnostics;
 
 namespace Phlogopite
 {
     public static class SpanBuilder
     {
-        public static SpanBuilder<T> Create<T>(T[] array)
+        public static SpanBuilder<T> Create<T>(Span<T> span)
         {
-            return new SpanBuilder<T>(array);
+            return new SpanBuilder<T>(span);
         }
 
-        public static SpanBuilder<T> Create<T>(T[] array, int offset, int count)
+        public static SpanBuilder<T> Create<T>(Span<T> span, int offset, int count)
         {
-            return new SpanBuilder<T>(array, offset, count);
+            return new SpanBuilder<T>(span, offset, count);
         }
     }
 
@@ -20,41 +19,36 @@ namespace Phlogopite
 
     public readonly ref struct SpanBuilder<T>
     {
-        private readonly T[] _array;
-        private readonly int _offset;
+        private readonly Span<T> _span;
         private readonly int _count;
 
-        public SpanBuilder(T[] array)
+        public SpanBuilder(Span<T> span)
         {
-            if (array is null)
-                ThrowHelper.ThrowArgumentNullException(ExceptionArgument.array);
-
-            Debug.Assert(array != null, "array != null");
-
-            _array = array;
-            _offset = 0;
-#pragma warning disable CA1062 // Validate arguments of public methods
-            _count = array.Length;
-#pragma warning restore CA1062 // Validate arguments of public methods
+            _span = span;
+            _count = span.Length;
         }
 
-        public SpanBuilder(T[] array, int offset, int count)
+        public SpanBuilder(Span<T> span, int offset, int count)
         {
-            if (array is null || (uint)offset > (uint)array.Length || (uint)count > (uint)(array.Length - offset))
-                ThrowHelper.ThrowArraySegmentCtorValidationFailedExceptions(array, offset, count);
+            if ((uint)offset > (uint)span.Length || (uint)count > (uint)(span.Length - offset))
+                ThrowHelper.ThrowArraySegmentCtorValidationFailedExceptions(span.Length, offset, count);
 
-            _array = array;
-            _offset = offset;
+            _span = span.Slice(offset);
             _count = count;
         }
 
-        public int Capacity => _array.Length - _offset;
+        public int Capacity => _span.Length;
 
         public int Count => _count;
 
         public bool IsEmpty => _count == 0;
 
 #pragma warning disable CA2225 // Operator overloads have named alternates
+
+        public static implicit operator SpanBuilder<T>(Span<T> span)
+        {
+            return new SpanBuilder<T>(span);
+        }
 
         public static implicit operator SpanBuilder<T>(T[] array)
         {
@@ -68,18 +62,18 @@ namespace Phlogopite
 
         public static implicit operator ReadOnlySpan<T>(SpanBuilder<T> spanBuilder)
         {
-            return new Span<T>(spanBuilder._array, spanBuilder._offset, spanBuilder._count);
+            return spanBuilder._span.Slice(0, spanBuilder._count);
         }
 
 #pragma warning restore CA2225 // Operator overloads have named alternates
 
-        public ref readonly T this[int index] => ref _array[_offset + index];
+        public ref readonly T this[int index] => ref _span[index];
 
         // ReSharper disable InconsistentNaming
 
         public ReadOnlySpan<T> AsSpan()
         {
-            return new ReadOnlySpan<T>(_array, _offset, _count);
+            return _span.Slice(0, _count);
         }
 
         public ReadOnlySpan<T> AsSpan(int start)
@@ -87,7 +81,7 @@ namespace Phlogopite
             if ((uint)start > (uint)_count)
                 ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.start);
 
-            return new ReadOnlySpan<T>(_array, _offset + start, _count - start);
+            return _span.Slice(start, _count - start);
         }
 
         public ReadOnlySpan<T> AsSpan(int start, int length)
@@ -98,21 +92,21 @@ namespace Phlogopite
             if ((uint)length > (uint)(_count - start))
                 ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.length);
 
-            return new ReadOnlySpan<T>(_array, _offset + start, length);
+            return _span.Slice(start, length);
         }
 
         // ReSharper restore InconsistentNaming
 
         public bool TryAppend(T item, out SpanBuilder<T> result)
         {
-            if (_offset + _count >= _array.Length)
+            if (_count >= _span.Length)
             {
                 result = this;
                 return false;
             }
 
-            _array[_offset + _count] = item;
-            result = new SpanBuilder<T>(_array, _offset, _count + 1);
+            _span[_count] = item;
+            result = _span.Slice(0, _count + 1);
             return true;
         }
 
@@ -123,7 +117,7 @@ namespace Phlogopite
 
         public bool Equals(SpanBuilder<T> other)
         {
-            return _array.Equals(other._array) && _offset == other._offset && _count == other._count;
+            return _span == other._span && _count == other._count;
         }
 
         public override bool Equals(object obj)
@@ -133,13 +127,7 @@ namespace Phlogopite
 
         public override int GetHashCode()
         {
-            unchecked
-            {
-                int hashCode = _array.GetHashCode();
-                hashCode = (hashCode * 397) ^ _offset;
-                hashCode = (hashCode * 397) ^ _count;
-                return hashCode;
-            }
+            return _count.GetHashCode();
         }
 
         public static bool operator ==(SpanBuilder<T> left, SpanBuilder<T> right)
